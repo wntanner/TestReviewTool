6d9515baed4107ef6b834424fcd5e00469d0644c
wntanner <32973177+wntanner@users.noreply.github.com>
2018-12-15 13:56:48 -0600

    mainframe comment

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,20 +2,26 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
+/**
+ * Holds onto a main panel and controls application sizing and exiting
+ * 
+ * @author William
+ *
+ */
 public class MainFrame extends JFrame {
-	
+
 	public static Dimension FRAMEDIM = new Dimension(800, 800);
-	
+
 	private MainPanel mp;
-	
+
 	public MainFrame() {
-		
+		// application settings
 		this.setResizable(true);
 		this.setLocationRelativeTo(null);
 		this.setPreferredSize(FRAMEDIM);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
+		// adding the mainpanel for display
 		mp = new MainPanel();
 		this.add(mp);
 		this.pack();
